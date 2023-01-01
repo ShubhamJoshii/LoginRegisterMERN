dfc46fab2ec1647a2b7b3981e0293a237cebbd73
Shubham Joshi <106878629+ShubhamJoshii@users.noreply.github.com>
2023-01-01 15:50:37 +0530

    Add files via upload

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    SECRET_KEY : "SHUBAHMJOSHIISAGOODBOYQWERTYUIOPAS",
+    DataBase : "mongodb+srv://MernProject:ske5QoqNrtXpd4yE@mernproject.y30y9v4.mongodb.net/MernProject?retryWrites=true&w=majority"
+}
