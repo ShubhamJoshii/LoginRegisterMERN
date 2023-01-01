a5b882f9a27bd311462f588c73909837c527e112
Shubham Joshi <106878629+ShubhamJoshii@users.noreply.github.com>
2023-01-01 15:43:01 +0530

    Delete dev.js

--- config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    SECRET_KEY:"SHUBAHMJOSHIISAGOODBOYQWERTYUIOPAS",
-    DataBase : "mongodb+srv://MernProject:ske5QoqNrtXpd4yE@mernproject.y30y9v4.mongodb.net/MernProject?retryWrites=true&w=majority"
-}
