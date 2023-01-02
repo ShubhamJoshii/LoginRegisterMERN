ba9f2b2503e2764231e3e18d5adba54fd9a1dd0f
ShubhamJoshii <shubhamjoshii676@gmail.com>
2023-01-02 21:51:25 +0530

    About CSS Fix

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import "./About.css"
 import profile from "../img/profile.png"
 import shubham from "../img/Shubham.jpg"
+import facebook from "../img/facebookk.png"
+import instagram from "../img/Insta.png"
+import linkedin from "../img/Linkedin.png"
+import github from "../img/github.png"
+import twitter from "../img/Twitter.png"
+
 import { useEffect } from 'react'
 import { useState } from 'react'
 import {useNavigate} from "react-router-dom";
@@ -40,11 +46,13 @@ function About() {
         <button className='picChange'>Upload Photo</button>
       {/* </div> */}
         <h3>Social Media</h3>
-        <a href={cokkiesData.instagram} target="_blank">Instagram</a>
-        <a href={cokkiesData.facebook} target="_blank">Facebook</a>
-        <a href={cokkiesData.github} target="_blank">Github</a>
-        <a href={cokkiesData.linkedin} target="_blank">Linkedin</a>
-        <a href={cokkiesData.twitter} target="_blank">Twitter</a>
+        <div>
+          <a href={cokkiesData.instagram} target="_blank" rel="noreferrer"> <img src={instagram} alt="" width="15px"/> Instagram</a>
+          <a href={cokkiesData.facebook} target="_blank" rel="noreferrer"> <img src={facebook} alt="" width="15px"/> Facebook</a>
+          <a href={cokkiesData.github} target="_blank" rel="noreferrer"> <img src={github} alt="" width="15px" /> Github</a>
+          <a href={cokkiesData.linkedin} target="_blank" rel="noreferrer"> <img src={linkedin} alt="" width="15px"/> Linkedin</a>
+          <a href={cokkiesData.twitter} target="_blank" rel="noreferrer"> <img src={twitter} alt="" width="15px"/> Twitter</a>
+        </div>
       </div>
       <div className='About2Half'>
         <div>
