e3cf3b0b952c46a250228226e901ae052f15828a
ShubhamJoshii <shubhamjoshii676@gmail.com>
2023-01-10 11:42:11 +0530

    Routing Fix React

--- index.js
+++ index.js
@@ -9,14 +9,17 @@ app.use(cookieParser())
 
 //Without This req.body will be null
 app.use(express.json());
-
+app.use(express.static(path.resolve(__dirname,"frontend/build")));
 // if(process.env.NODE_ENV == "Production"){
     
-    app.get("/",(req,res)=>{
-        app.use(express.static(path.resolve(__dirname,"frontend/build")));
-        res.status(200).sendFile(path.resolve(__dirname,"frontend/build"));  
-    })
-// }
+app.get("/",(req,res)=>{
+    res.status(200).sendFile(path.resolve(__dirname,"frontend/build"));  
+})
+    // }
+    
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
+});
 
 const User = require("./Database")
 
